4fce7fde99238b9f399a860eba6465ab9d402225
Giang Nguyen <hoanggia@uni-bremen.de>
2023-07-26 11:15:36 +0200

    Add new branch

--- setup_ros_packages.bash
+++ setup_ros_packages.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd $(dirname $0)/images/Multiverse-image
 if [ ! -d "Multiverse" ]; then
-    git clone git@github.com:Multiverse-Framework/Multiverse.git
+    git clone git@github.com:Multiverse-Framework/Multiverse.git --branch docker
 else
-    (cd Multiverse && git pull)
+    (cd Multiverse && git checkout docker && git pull origin docker)
 fi
 wstool update
