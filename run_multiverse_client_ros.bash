804fd3e561fd7d906e5d1a7b252e7fb9e738448c
Giang Nguyen <hoanggia@uni-bremen.de>
2023-07-27 17:06:23 +0200

    Update

--- run_multiverse_client_ros.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-xhost +local:root
-docker compose up multiverse-client-ros-service
-
-# cleanup after multiverse-client-ros-service has been closed
-xhost -local:root
---
+++ run_rviz.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+xhost +local:root
+docker compose up rviz-service
+
+# cleanup after rviz-service has been closed
+xhost -local:root
