a9baa8a060c492861d4827ed734a7902d57a0a4d
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-08-08 09:57:28 +0200

    add interactive marker and marker array

---
+++ binder/demo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source /home/Multiverse/multiverse_ws/devel/setup.bash && \
+  roslaunch multiverse_server multiverse_server.launch &
+
+source /home/Multiverse/multiverse_ws/devel/setup.bash && \
+  roslaunch tiago_dual_in_apartment mujoco.launch headless:=true &
+
+source /home/Multiverse/multiverse_ws/devel/setup.bash && \
+  roslaunch tiago_dual_in_apartment multiverse_client.launch &
+
+source /home/Multiverse/giskard_ws/devel/setup.bash && \
+  roslaunch tiago_dual_control tiago_dual_giskard.launch &
+
+if [ "$DISPLAY" == ":99" ]; then
+  echo "No Display connected !!!"
+else
+  source /home/Multiverse/giskard_ws/devel/setup.bash && \
+    roslaunch tiago_dual_in_apartment rviz.launch &
+fi
+
+source /home/Multiverse/giskard_ws/devel/setup.bash && \
+  roslaunch rvizweb update_config.launch config_file:=/home/binder/rvizweb-config.json && \
+  rosrun interactive_marker_proxy proxy topic_ns:=/eef_control target_frame:=/base_link &
+
+wait
--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 source /home/Multiverse/giskard_ws/devel/setup.bash
 
-roscore & 
+roscore &
 roslaunch --wait rvizweb rvizweb.launch &
-roslaunch --wait rosboard rosboard.launch &
-jupyter lab workspaces import /home/${NB_USER}/repo/binder/iros.jupyterlab-workspace
-exec "$@"
+
+# Use xvfb virtual display when there is no display connected.
+if [ -n "$DISPLAY" ]; then
+    exec "$@"
+else
+    xvfb-run exec "$@"
+fi
