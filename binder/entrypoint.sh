05aa97b53e3eb416f508f762febcc3e22d514f56
Giang Nguyen <hoanggia@uni-bremen.de>
2023-09-12 11:39:10 +0200

    Update binder

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source /home/Multiverse/giskard_ws/devel/setup.bash
 
-roscore &
-roslaunch --wait rvizweb rvizweb.launch &
-
 # Use xvfb virtual display when there is no display connected.
 if [ -n "$DISPLAY" ]; then
     exec "$@"
