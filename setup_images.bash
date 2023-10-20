a186c427a180d957be6dcf7ce5642bca64018fa3
Giang Nguyen <hoanggia@uni-bremen.de>
2023-10-20 13:33:52 +0200

    Update docker

--- setup_images.bash
+++ setup_images.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 (cd $(dirname $0)/images/MultiverseCore-image; wstool update; git submodule update --init)
+if [ ! -d "$(dirname $0)/images/Multiverse-image/Multiverse" ]; then
+    (mkdir $(dirname $0)/images/Multiverse-image/Multiverse)
+    (cp -r $(dirname $0)/images/MultiverseCore-image/Multiverse/multiverse_ws $(dirname $0)/images/Multiverse-image/Multiverse/multiverse_ws)
+    (cp $(dirname $0)/images/MultiverseCore-image/Multiverse/build_multiverse_ws.sh $(dirname $0)/images/Multiverse-image/Multiverse/build_multiverse_ws.sh)
+fi
 (cd $(dirname $0)/images/MultiverseDemos-image; wstool update)
 (cd $(dirname $0)/images/Giskard-image; wstool update)
