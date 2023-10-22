9feb8e9fbf059b65ac093c21ae272eb94973cbc0
Giang Nguyen <hoanggia@uni-bremen.de>
2023-10-22 18:03:18 +0200

    Update docker

--- binder/testbed_demo_entrypoint.sh
+++ binder/testbed_demo_entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 source /home/Multiverse/giskard_ws/devel/setup.bash
 
+multiverse_server &
 roscore & 
-roslaunch --wait rvizweb rvizweb.launch &
-jupyter lab workspaces import /home/binder/main.jupyterlab-workspace
+roslaunch --wait rvizweb rvizweb.launch config_file:=/home/binder/testbed_demo_rvizweb_config.json &
+roslaunch --wait tiago_dual_in_testbed demo.launch &
+jupyter lab workspaces import /home/binder/testbed_demo.jupyterlab-workspace
 exec "$@"
