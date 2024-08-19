b2584d5e4494dd910e0e63a0fff364ee570264e6
Giang Nguyen <hoanggia@uni-bremen.de>
2024-08-19 15:09:41 +0200

    Update Dockerfile

--- setup_images.bash
+++ setup_images.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d $(dirname $0)/images/Multiverse-image/Multiverse ]; then
-    git clone https://github.com/Multiverse-Framework/Multiverse $(dirname $0)/images/Multiverse-image/Multiverse
+    git clone https://github.com/Multiverse-Framework/Multiverse $(dirname $0)/images/Multiverse-image/Multiverse --depth 1
 else
     cd $(dirname $0)/images/Multiverse-image/Multiverse
     git pull
