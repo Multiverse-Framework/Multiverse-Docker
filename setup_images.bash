022593dbbaf27e13d08d11cc9d57869bd515aec1
Giang Nguyen <hoanggia@uni-bremen.de>
2025-03-21 17:35:20 +0100

    Update docker

--- setup_images.bash
+++ setup_images.bash
@@ -7,7 +7,7 @@ else
     (cd $MULTIVERSE_DIR; git pull)
 fi
 
-(cd $MULTIVERSE_DIR; git submodule update --init multiverse/resources; cd multiverse/resources; git submodule update --init)
+(cd $MULTIVERSE_DIR; git submodule update --init --depth 1 multiverse/resources; cd multiverse/resources; git submodule update --init)
 
 MULTIVERSE_TUTORIALS_DIR=$(dirname $0)/images/Multiverse-Tutorials-image/Multiverse-Tutorials
 if [ ! -d $MULTIVERSE_TUTORIALS_DIR ]; then
